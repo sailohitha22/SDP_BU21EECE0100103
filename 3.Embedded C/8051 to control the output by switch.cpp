d3511f67a0d74512d80fdc72293a3de9c4eecae1
sailohitha22 <saraisailohitha22@gmail.com>
2024-06-12 14:33:04 +0530

    Update 8051 to control the output by switch.cpp

--- 3.Embedded C/8051 to control the output by switch.cpp
+++ 3.Embedded C/8051 to control the output by switch.cpp
@@ -1,40 +1,52 @@
-#include <reg51.h>
-sbit sw1 = P2^1;
-sbit sw2 = P2^2;
-void delay(unsigned int);
+Objective:
+- The objective of this code seems to be controlling the behavior of the output port P1 based on the states of two switches (sw1 and sw2).
+- When both switches are off (sw1 == 0 and sw2 == 0), P1 is set to all 0s.
+- When sw1 is off and sw2 is on (sw1 == 0 and sw2 == 1), P1 alternates between 0xAA and 0x55.
+- When sw1 is on and sw2 is off (sw1 == 1 and sw2 == 0), P1 alternates between 0x55 and 0xAA.
+- When both switches are on (sw1 == 1 and sw2 == 1), P1 alternates between 0x55 and 0xAA with a delay of 5000 iterations of the delay function.
+
+#include <reg51.h>  // Include the header file for 8051 microcontroller register definitions
+
+sbit sw1 = P2^1;  // Define sw1 as a single bit variable at pin P2.1
+sbit sw2 = P2^2;  // Define sw2 as a single bit variable at pin P2.2
+
+void delay(unsigned int);  // Function prototype declaration for delay function
+
 void main(void)
 {
-	sw1 = 0;
-	sw2 = 0;
-	while(1)
-	{
-		if(sw1==0 && sw2==0)
-		{
-			P1=0x00;
-		}
-			else if (sw1==0 && sw2==1)
-			{
-				P1=0xAA;
-			}
-			else if (sw1==1 && sw2==0)	
-			{
-				P1=0x55;
-			}
-			else if (sw1==1 && sw2==1)	
-			{
-			//while(1)   // repeat forever
-				//{
-					P1=0x55; // toggle all bits of P1
-					delay(5000); // calling delay
-					P1=0xAA; // toggle all bits of P1
-					delay(5000); // calling delay
-				//}
-			}
-		}
-	}
+    sw1 = 0;  // Initialize sw1 to 0
+    sw2 = 0;  // Initialize sw2 to 0
+    
+    while(1)  // Infinite loop
+    {
+        if(sw1 == 0 && sw2 == 0)
+        {
+            P1 = 0x00;  // If both switches are off, set P1 to all 0s
+        }
+        else if (sw1 == 0 && sw2 == 1)
+        {
+            P1 = 0xAA;  // If sw1 is off and sw2 is on, set P1 to 0xAA (alternating bits)
+        }
+        else if (sw1 == 1 && sw2 == 0)	
+        {
+            P1 = 0x55;  // If sw1 is on and sw2 is off, set P1 to 0x55 (alternating bits)
+        }
+        else if (sw1 == 1 && sw2 == 1)	
+        {
+            // If both switches are on, alternate between 0x55 and 0xAA with a delay
+            P1 = 0x55;  // Set P1 to 0x55
+            delay(5000);  // Call delay function
+            P1 = 0xAA;  // Set P1 to 0xAA
+            delay(5000);  // Call delay function
+        }
+    }
+}
+
 void delay(unsigned int time)
 {
-	unsigned int i, j;
-	for (i=0;i<time;i++)
-		for (j=0;j<1275;j++);
+    unsigned int i, j;
+    for (i = 0; i < time; i++)
+        for (j = 0; j < 1275; j++);  // Nested loop for creating delay
 }
+```
+
